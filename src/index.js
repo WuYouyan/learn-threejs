795c79622faa75729e971a3075b6b85e4709e065
Youyan WU <youyan.wu@savoye.com>
2021-04-18 21:06:27 +0200

    add detection for webgl support

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import * as THREE from "../lib/three/three.module.js";
+// import * as THREE from "../lib/three/three.module.js";
+import * as THREE from "../node_modules/three/build/three.module.js";
+import { WEBGL} from "../node_modules/three/examples/jsm/WebGL.js";
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -12,10 +15,17 @@ const cube = new THREE.Mesh(geometry, material);
 
 scene.add( cube );
 camera.position.z = 5;
+
 function animate() {
 	requestAnimationFrame( animate );
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
-animate();
+if (WEBGL.isWebGLAvailable()) {
+    // Initiate function or other initializations here
+    animate();
+} else {
+    const warning = WEBGL.getWebGLErrorMessage();
+    document.getElementById('container').appendChild(warning);
+}
